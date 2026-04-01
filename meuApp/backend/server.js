d7e710fc9c50e3f808d81598e8f82e963363a4fa
giovanni <coisas-2020@hotmail.com>
2026-04-01 00:46:26 +0000

    É o vibes e os codas né pai

--- meuApp/backend/server.js
+++ meuApp/backend/server.js
@@ -1,25 +1,40 @@
-const express = require('express'); 
+const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const Tarefa = require('./models/Tarefa');
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: '*'
+}));
+
 app.use(express.json());
 
 mongoose.connect(
-    'mongodb+srv://dbUser:Banguela1@cluster0.aecfpvj.mongodb.net/?appName=Cluster0',
-    { useNewUrlParser:true, useUnifiedTopology:true }
+  'mongodb+srv://dbUser:Banguela1@cluster0.aecfpvj.mongodb.net/tarefas?retryWrites=true&w=majority',
 )
-.then(()=>console.log('MongoDB Atlas conectado'))
-.catch(err=>console.log(err));
+.then(() => console.log('MongoDB Atlas conectado'))
+.catch(err => console.log(err));
 
-app.post('/tarefas', async(req,res)=>{ const t=await Tarefa.create(req.body);
-    res.json(t) })
-app.get('/tarefas', async(req,res)=>{ const t=await Tarefa.find(); res.json(t) })
-app.put('/tarefas/:id', async(req,res)=>{ const t=await
-Tarefa.findByIdAndUpdate(req.params.id,req.body,{new:true}); res.json(t) })
-app.delete('/tarefas/:id', async(req,res)=>{ await
-Tarefa.findByIdAndDelete(req.params.id); res.json({ok:true}) })
+app.post('/tarefas', async (req,res) => {
+  const t = await Tarefa.create(req.body);
+  res.json(t);
+});
 
-app.listen(3000,()=>console.log('Servidor rodando'));
+app.get('/tarefas', async (req,res) => {
+  const t = await Tarefa.find();
+  res.json(t);
+});
+
+app.put('/tarefas/:id', async (req,res) => {
+  const t = await Tarefa.findByIdAndUpdate(req.params.id, req.body, { new:true });
+  res.json(t);
+});
+
+app.delete('/tarefas/:id', async (req,res) => {
+  await Tarefa.findByIdAndDelete(req.params.id);
+  res.json({ ok:true });
+});
+
+app.listen(3000, '0.0.0.0', () => console.log('Servidor rodando'));
